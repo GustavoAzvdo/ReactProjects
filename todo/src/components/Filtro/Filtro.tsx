46c4f1e3e9262e4ea96028a2e36bd9bf9b2b4741
Gustavo Azevedo <ghca12@gmail.com>
2025-07-23 19:33:57 -0300

    fix/ excluindo variaveis que nao estao sendo usadas

--- todo/src/components/Filtro/Filtro.tsx
+++ todo/src/components/Filtro/Filtro.tsx
@@ -12,7 +12,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 //data pt-br
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br'
 dayjs.locale('pt-br')
 
